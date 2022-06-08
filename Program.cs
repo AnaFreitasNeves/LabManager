8634776104cc8c7a8feda092cf91b4bb3c3d7541
Ana Carolina <freitas.neves@aluno.ifsp.edu.br>
2022-06-08 11:51:41 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -37,6 +37,40 @@ internal class Program
                 var computer = new Computer(id, ram, processor);
                 computerRepository.Save(computer);
             }
+            if(modelAction == "Show") 
+            {
+                var id = Convert.ToInt32(args[2]);
+                 if(computerRepository.ExistsById(id))
+                {
+                var computer = computerRepository.GetById(id);
+                Console.WriteLine($"{computer.Id}, {computer.Ram}, {computer.Processor}");
+                }
+                else
+                {
+                Console.WriteLine($"O Computador {id} não existe");
+                }
+            }
+
+            if(modelAction == "Delete") 
+            {
+                var id = Convert.ToInt32(args[2]);
+
+                 computerRepository.Delete(id);
+
+                
+            }
+            if (modelAction == "Update")
+            {
+                var id = Convert.ToInt32(args[2]);
+                var ram = args[3];
+                var processor = args[4];
+
+                var computer = new Computer(id, ram, processor);
+
+                computerRepository.Update(computer);
+            }
         }
     }
+
+  
 }
--- Repositories/ComputerRepositories.cs
+++ Repositories/ComputerRepositories.cs
@@ -27,11 +27,8 @@ class ComputerRepository
 
         while(reader.Read())
         {
-            var id = reader.GetInt32(0);
-            var ram = reader.GetString(1);
-            var processor = reader.GetString(2);
-            var computer = new Computer(id, ram, processor);
-            computers.Add(computer);
+           var computer = ReaderToComputer(reader);
+           computers.Add(computer);
         }
 
         connection.Close();
@@ -53,5 +50,78 @@ class ComputerRepository
         command.ExecuteNonQuery();
         connection.Close();
     }
+        public Computer GetById(int id)
+        {
 
+            var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = " SELECT * FROM Computers WHERE id=$id";
+            command.Parameters.AddWithValue("$id", id);
+
+            var reader = command.ExecuteReader();
+            reader.Read();
+
+            var computer = ReaderToComputer(reader);
+                          
+
+            return computer;
+        }
+        public void Delete(int id)
+        {
+             var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+            connection.Open();
+
+             var command = connection.CreateCommand();
+             command.CommandText = " DELETE FROM Computers WHERE id=$id";
+             command.Parameters.AddWithValue("$id", id);
+
+             command.ExecuteNonQuery();
+             connection.Close();
+
+        }
+        public Computer Update(Computer computer)
+        {
+            var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = @" UPDATE Computers SET ram=$ram, processor=$processor WHERE id=$id;";
+            command.Parameters.AddWithValue("$id", computer.Id);
+            command.Parameters.AddWithValue("$ram", computer.Ram);
+            command.Parameters.AddWithValue("$processor", computer.Processor);
+
+            return computer;
+        }
+
+        public bool ExistsById(int id)
+        {
+             var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+            connection.Open();
+
+            var command = connection.CreateCommand();
+             command.CommandText = " SELECT count(id) FROM Computers WHERE id=$id";
+             command.Parameters.AddWithValue("$id", id);
+
+           // var reader = command.ExecuteReader();
+           //  reader.Read();
+           // var result = reader.GetBoolean(0);
+
+             bool result = Convert.ToBoolean(command.ExecuteScalar());
+            connection.Close();
+
+            return result;
+        }
+
+        private Computer ReaderToComputer(SqliteDataReader reader)
+        {
+             var computer = new Computer
+                 (reader.GetInt32(0), 
+                  reader.GetString(1), 
+                  reader.GetString(2)
+                 );
+
+            return computer;
+        }
 }
